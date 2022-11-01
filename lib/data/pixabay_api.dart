61c09c1eb1acc51e2db76725382d89e8c14bae24
kkimwooo <kkimwooo@gmail.com>
2022-11-01 22:02:09 +0900

    Mockito 활용한 외부 API TEST
    실제 API와의 종속성을 제거하고 결과를 가정해서 테스트를 하기 위함.
    실제 서비스의 API로 테스트 하는 것은 테스트 성능 저하나 실제 서버의 이상유무에 따라 값이 달라질 수도 있고, 모든 성공 실패를 테스트 하기 어려움
    이런 경우 Mock 객체를 통해 다양한 경우를 테스트할 수 있음.

--- lib/data/pixabay_api.dart
+++ lib/data/pixabay_api.dart
@@ -1,19 +1,18 @@
 import 'dart:convert';
-import 'dart:io';
 import 'package:about_flutter_clean_architecture/data/photo_api_repository.dart';
 import 'package:about_flutter_clean_architecture/model/photo.dart';
 import 'package:http/http.dart' as http;
 
 class PixabayApi implements PhotoApiRepository {
-  final baseUrl = 'https://pixabay.com/api/';
-  final apiKey = '24999262-d78d2a61a8beebcf2664f2cde';
+  static const String baseUrl = 'https://pixabay.com/api/';
+  static const String apiKey = '24999262-d78d2a61a8beebcf2664f2cde';
 
   @override
   Future<List<Photo>> fetch(String query, {http.Client? client}) async {
     //client 가 null 이면 http.Client() 할당
     client ??= http.Client();
 
-    final res = await http.get(Uri.parse('$baseUrl?key=$apiKey&q=$query&image_type=photo'));
+    final res = await client.get(Uri.parse('$baseUrl?key=$apiKey&q=$query&image_type=photo'));
     Map<String, dynamic> jsonRes = jsonDecode(res.body);
     Iterable hits = jsonRes['hits']; //Iterable로 받아서 반복 처리 용이
     return hits.map((e) => Photo.fromJson(e)).toList();
